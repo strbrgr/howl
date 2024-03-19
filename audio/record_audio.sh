bf88840e90ad5b20c22227cad87242ba2157d59a
Jochen Stierberger <jochen@stierberger.com>
2024-03-19 18:21:38 -0500

    modify export path

--- audio/record_audio.sh
+++ audio/record_audio.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-timeout 28800 /usr/bin/arecord -D plughw:2,0 -f cd -t wav --max-file-time 3600 --use-strftime "/home/jo/Desktop/howl/audio/%Y/%m/%d/listen-%H-%M-%v.wav"
+timeout 28800 /usr/bin/arecord -D plughw:2,0 -f cd -t wav --max-file-time 3600 --use-strftime "/home/jo/Desktop/howl/audio/export/%Y/%m/%d/listen-%H-%M-%v.wav"
